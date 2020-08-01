3f8fbcf9298fb3d0a6c8b9d77d226cecaba10b53
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-08-01 12:22:54 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Stormbreaker by baibhab
+kernel.string=Stormbreaker by sparxfusion
 do.devicecheck=0
 do.modules=0
 do.systemless=1
@@ -20,7 +20,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/by-name/boot;
+block=/dev/block/platform/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
